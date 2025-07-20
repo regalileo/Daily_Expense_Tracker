3da0a169f18b227f82499445bcecd8d6206c9135
regalileo <146832616+regalileo@users.noreply.github.com>
2025-07-20 12:20:26 +0700

    Update transactionManager.js

--- js/transactionManager.js
+++ js/transactionManager.js
@@ -1,4 +1,4 @@
-import { formatRupiah, saveToStorage, loadFromStorage } from './utils.js';
+import { formatRupiah, saveToStorage, loadFromStorage } from 'js/utils.js';
 
 export default class TransactionManager {
   constructor(storageKey = 'transactions') {
