3542c329d2cdea28a0214fdafa09b4916bf99607
regalileo <146832616+regalileo@users.noreply.github.com>
2025-07-20 12:06:53 +0700

    Update chartManager.js

--- js/chartManager.js
+++ js/chartManager.js
@@ -1,41 +1,95 @@
-import { formatRupiah } from "./utils.js";
+import { formatRupiah } from './utils.js';
 
-export class ChartManager {
-  constructor() {
-    this.categoryCtx = document.getElementById("categoryChart");
-    this.dateCtx = document.getElementById("dateChart");
-    this.categoryChart = null;
-    this.dateChart = null;
+export default class ChartManager {
+  constructor(ctxBar, ctxPie) {
+    this.ctxBar = ctxBar;
+    this.ctxPie = ctxPie;
+    this.barChart = null;
+    this.pieChart = null;
   }
 
-  renderCategoryChart(data) {
-    const labels = Object.keys(data);
-    const values = Object.values(data);
-    this.categoryChart?.destroy();
-    this.categoryChart = new Chart(this.categoryCtx, {
-      type: "pie",
-      data: {
-        labels,
-        datasets: [{ data: values }]
-      },
-    });
-  }
+  renderBarChart(dataByCategory) {
+    const labels = Object.keys(dataByCategory);
+    const data = Object.values(dataByCategory);
 
-  renderDateChart(data) {
-    const labels = Object.keys(data);
-    const values = Object.values(data);
-    this.dateChart?.destroy();
-    this.dateChart = new Chart(this.dateCtx, {
-      type: "line",
+    if (this.barChart) this.barChart.destroy();
+
+    this.barChart = new Chart(this.ctxBar, {
+      type: 'bar',
       data: {
         labels,
         datasets: [{
-          label: "Pengeluaran Harian",
-          data: values,
-          borderColor: "green",
-          fill: false
+          label: 'Pengeluaran/Kategori',
+          data,
+          backgroundColor: this._generateColors(labels.length),
         }]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: { display: false },
+          tooltip: {
+            callbacks: {
+              label: context => formatRupiah(context.parsed.y)
+            }
+          }
+        },
+        scales: {
+          y: {
+            ticks: {
+              callback: value => formatRupiah(value)
+            }
+          }
+        }
       }
     });
+  }
+
+  renderPieChart(dataByCategory) {
+    const labels = Object.keys(dataByCategory);
+    const data = Object.values(dataByCategory);
+
+    if (this.pieChart) this.pieChart.destroy();
+
+    this.pieChart = new Chart(this.ctxPie, {
+      type: 'doughnut',
+      data: {
+        labels,
+        datasets: [{
+          label: 'Distribusi Pengeluaran',
+          data,
+          backgroundColor: this._generateColors(labels.length),
+        }]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'bottom',
+            labels: {
+              color: '#ccc',
+              font: { size: 14 }
+            }
+          },
+          tooltip: {
+            callbacks: {
+              label: context => `${context.label}: ${formatRupiah(context.parsed)}`
+            }
+          }
+        }
+      }
+    });
+  }
+
+  _generateColors(count) {
+    const baseColors = [
+      '#00b894', '#6c5ce7', '#fd79a8', '#e17055', '#fab1a0',
+      '#0984e3', '#ffeaa7', '#d63031', '#55efc4', '#a29bfe'
+    ];
+    const result = [];
+    for (let i = 0; i < count; i++) {
+      result.push(baseColors[i % baseColors.length]);
+    }
+    return result;
   }
 }
