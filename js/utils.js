4519648d8ba3d91169f42e837b4392a7cf22c731
regalileo <146832616+regalileo@users.noreply.github.com>
2025-07-20 11:59:27 +0700

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,12 @@
-export function formatRupiah(amount) {
-    return new Intl.NumberFormat('id-ID', {
-    style: 'currency',
-    currency: 'IDR',
-}).format(amount);
-}
+export const formatRupiah = (num) => {
+  return "Rp " + num.toLocaleString("id-ID");
+};
 
+export const saveToStorage = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const getFromStorage = (key, defaultVal = null) => {
+  const data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : defaultVal;
+};
